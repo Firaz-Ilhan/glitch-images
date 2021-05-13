31c71b88262c0ed894bef0fc66a558e3b279a277
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-05-13 14:56:44 +0200

    add convert_to_jpeg_progressive()

--- glitch.py
+++ glitch.py
@@ -1,6 +1,8 @@
 import argparse
 import random
 
+from PIL import Image  # pip install Pillow
+
 parser = argparse.ArgumentParser(description="glitch images")
 
 # arguments
@@ -8,6 +10,11 @@ parser.add_argument("-i", "--input", help="input file path")
 parser.add_argument("-o", "--output", help="output file path")
 
 args = parser.parse_args()
+
+
+def convert_to_jpeg_progressive(input_path, output_path):
+    image = Image.open(input_path).convert('RGB')
+    image.save(output_path, 'jpeg', optimize=True, progressive=True)
 
 
 def read_file(input_path):
@@ -28,6 +35,7 @@ def write_file(output_path, manipulated_bytes):
 
 
 if __name__ == '__main__':
-    byte_array = read_file(args.input)
+    convert_to_jpeg_progressive(args.input, args.output)
+    byte_array = read_file(args.output)
     byte_array = manipulate_bytes(byte_array)
     write_file(args.output, byte_array)
