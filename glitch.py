4dbee5e2aee1e59cd79f401d367583789f52d883
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-05-21 22:00:53 +0200

    add create_gif()

--- glitch.py
+++ glitch.py
@@ -1,5 +1,7 @@
 import argparse
+import io
 import random
+import sys
 
 from PIL import Image  # pip install Pillow
 
@@ -22,11 +24,15 @@ def read_file(input_path):
         return bytearray(i.read())
 
 
-def manipulate_bytes(byte_array):
-    for i in range(0, len(byte_array), 8):
-        if byte_array[i] == 254:
-            byte_array[i] = random.randint(63, 254)
-    return byte_array
+def manipulate_bytes(by, skip=8):
+    by_clone = bytearray.copy(by)
+    seed_value = random.randrange(sys.maxsize)
+    random.seed(seed_value)
+    for i in range(0, len(by_clone), skip):
+        if by_clone[i] == 221:  # 221
+            rnd = random.randint(128, 254)
+            by_clone[i] = rnd
+    return by_clone
 
 
 def write_file(output_path, manipulated_bytes):
@@ -34,8 +40,21 @@ def write_file(output_path, manipulated_bytes):
         o.write(manipulated_bytes)
 
 
+def create_gif(output_path, byte_array):
+    img1 = manipulate_bytes(byte_array)
+    img2 = manipulate_bytes(byte_array)
+
+    image1 = Image.open(args.output)
+    image2 = Image.open(io.BytesIO(img1))
+    image3 = Image.open(io.BytesIO(img2))
+
+    image1.save(output_path, save_all=True, append_images=[image2, image3], duration=250, loop=0)
+
+
 if __name__ == '__main__':
     convert_to_jpeg_progressive(args.input, args.output)
-    byte_array = read_file(args.output)
-    byte_array = manipulate_bytes(byte_array)
-    write_file(args.output, byte_array)
+    # byte_array = read_file(args.output)
+    # byte_array1 = manipulate_bytes(byte_array)
+    # write_file(args.output, byte_array1)
+    a = read_file(args.output)
+    create_gif(args.output, a)
