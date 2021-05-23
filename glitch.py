ea45dc2ab6fd47b7f723c7a90410271dff8c0ac8
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-05-23 15:28:30 +0200

    refactor create_gif()

--- glitch.py
+++ glitch.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description="glitch images")
 # arguments
 parser.add_argument("-i", "--input", help="input file path")
 parser.add_argument("-o", "--output", help="output file path")
+parser.add_argument("-g", "--gif", type=int, help="generate gif")
 
 args = parser.parse_args()
 
@@ -24,15 +25,15 @@ def read_file(input_path):
         return bytearray(i.read())
 
 
-def manipulate_bytes(by, skip=8):
-    by_clone = bytearray.copy(by)
+def manipulate_bytes(bytes_array, skip=8):
+    bytes_clone = bytearray.copy(bytes_array)
     seed_value = random.randrange(sys.maxsize)
     random.seed(seed_value)
-    for i in range(0, len(by_clone), skip):
-        if by_clone[i] == 221:  # 221
+    for i in range(0, len(bytes_clone), skip):
+        if bytes_clone[i] == 221:
             rnd = random.randint(128, 254)
-            by_clone[i] = rnd
-    return by_clone
+            bytes_clone[i] = rnd
+    return bytes_clone
 
 
 def write_file(output_path, manipulated_bytes):
@@ -40,21 +41,25 @@ def write_file(output_path, manipulated_bytes):
         o.write(manipulated_bytes)
 
 
-def create_gif(output_path, byte_array):
-    img1 = manipulate_bytes(byte_array)
-    img2 = manipulate_bytes(byte_array)
-
+def create_gif(output_path, byte_array, number_of_images):
     image1 = Image.open(args.output)
-    image2 = Image.open(io.BytesIO(img1))
-    image3 = Image.open(io.BytesIO(img2))
+    all_images = []
 
-    image1.save(output_path, save_all=True, append_images=[image2, image3], duration=250, loop=0)
+    for i in range(number_of_images):
+        new_bytes = manipulate_bytes(byte_array)
+        new_image = Image.open(io.BytesIO(new_bytes))
+        all_images.append(new_image)
+        print("append", i)
+
+    print("saving gif...")
+    image1.save(output_path, save_all=True, append_images=all_images,
+                duration=250, optimize=True, loop=0)
+    print("gif saved")
 
 
 if __name__ == '__main__':
     convert_to_jpeg_progressive(args.input, args.output)
-    # byte_array = read_file(args.output)
     # byte_array1 = manipulate_bytes(byte_array)
     # write_file(args.output, byte_array1)
-    a = read_file(args.output)
-    create_gif(args.output, a)
+    original_bytes = read_file(args.output)
+    create_gif(args.output, original_bytes, args.gif)
