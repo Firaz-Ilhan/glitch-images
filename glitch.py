623867c0a87cf3f95032ff38cd1195dfb1afdff3
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-05-27 17:13:02 +0200

    add append_images() and skip argument

--- glitch.py
+++ glitch.py
@@ -11,6 +11,8 @@ parser = argparse.ArgumentParser(description="glitch images")
 parser.add_argument("-i", "--input", help="input file path", required=True)
 parser.add_argument("-o", "--output", help="output file path", required=True)
 parser.add_argument("-g", "--gif", type=int, help="generate gif")
+parser.add_argument("-s", "--skip", type=int, default=8,
+                    help="higher value means less glitch - Default is set to 8")
 
 args = parser.parse_args()
 
@@ -21,6 +23,9 @@ def convert_to_jpeg_progressive(input_path, output_path):
         image.save(output_path, 'jpeg', optimize=True, progressive=True)
     except FileNotFoundError:
         print(f"the file {input_path} does not exist")
+        raise SystemExit
+    except Exception:
+        print(f"could not save {output_path}, check permissions")
         raise SystemExit
 
 
@@ -33,7 +38,7 @@ def read_file(input_path):
         raise SystemExit
 
 
-def manipulate_bytes(bytes_array, skip=8):
+def manipulate_bytes(bytes_array, skip):
     bytes_clone = bytearray.copy(bytes_array)
     seed_value = random.randrange(sys.maxsize)
     random.seed(seed_value)
@@ -53,25 +58,33 @@ def write_file(output_path, manipulated_bytes):
         raise SystemExit
 
 
-def create_gif(output_path, byte_array, number_of_images):
-    image1 = Image.open(args.output)
+def append_images(byte_array, number_of_images):
     all_images = []
 
     for i in range(number_of_images):
-        new_bytes = manipulate_bytes(byte_array)
+        new_bytes = manipulate_bytes(byte_array, args.skip)
         new_image = Image.open(io.BytesIO(new_bytes))
         all_images.append(new_image)
         print("append image", i + 1)
 
+    return all_images
+
+
+def create_gif(output_path, all_images):
+    image1 = Image.open(args.output)
+
     print("saving gif...")
     image1.save(output_path, save_all=True, append_images=all_images,
                 duration=250, optimize=True, loop=0)
-    print(f"gif {output_path} saved")
+    print(f"{output_path} saved")
 
 
 if __name__ == '__main__':
     convert_to_jpeg_progressive(args.input, args.output)
-    # byte_array1 = manipulate_bytes(byte_array)
-    # write_file(args.output, byte_array1)
     original_bytes = read_file(args.output)
-    create_gif(args.output, original_bytes, args.gif)
+    if args.gif is None:
+        single_byte_array = manipulate_bytes(original_bytes, args.skip)
+        write_file(args.output, single_byte_array)
+    else:
+        images = append_images(original_bytes, args.gif)
+        create_gif(args.output, images)
